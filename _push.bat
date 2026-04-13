5cb946d092b07ba028fcfb3bffe0552f21b1281d
tecknohow <tecknohow@users.noreply.github.com>
2026-04-13 04:19:34 -0400

    chore: remove temp script

--- _push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d D:\devprojs\GitHub\TaskDream
-git add -A
-git commit -m "feat: initial TaskDream scaffold - Go backend + Vue 3 frontend"
-git push -u origin main
-echo PUSH_COMPLETE > D:\devprojs\GitHub\TaskDream\.push_status
